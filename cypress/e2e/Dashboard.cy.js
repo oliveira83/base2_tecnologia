9ebb82de36f235cc0e3186071e705aaa8a4acb37
oliveira83 <felipe.oliveira@gmail.com>
2024-07-12 01:25:52 -0300

    remoção de comentarios

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -7,24 +7,24 @@ describe('Mantis', () => {
         cy.login('Felipe_Oliveira', 'gdlp088');
     });
 
-    // it('Criar tarefa', () => {
-    //     cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.LINK_CRIAR_TAREFA).click();
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.CRIAR_TAREFA_LISTA).select('2');
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.FREQUENCIA_LISTA).select('10');
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.GRAVIDADE_LISTA).select('10');
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.PRIORIDADE_LISTA).select('40');
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.RESUMO).type('Teste');
-    //     cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.DESCRICAO).type('Teste');
+    it('Criar tarefa', () => {
+        cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.LINK_CRIAR_TAREFA).click();
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.CRIAR_TAREFA_LISTA).select('2');
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.FREQUENCIA_LISTA).select('10');
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.GRAVIDADE_LISTA).select('10');
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.PRIORIDADE_LISTA).select('40');
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.RESUMO).type('Teste');
+        cy.get(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.DESCRICAO).type('Teste');
 
-    //     const filePath = 'teste.txt';
+        const filePath = 'teste.txt';
 
-    //     cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.ENVIAR_ARQUIVOS).attachFile(filePath);
-    //     cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.BTN_CRIAR_TAREFA).click()
+        cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.ENVIAR_ARQUIVOS).attachFile(filePath);
+        cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.FORM_CRIAR_TAREFA.BTN_CRIAR_TAREFA).click()
 
-    //     cy.url().should('include', 'bug_report.php?posted=1')
-    //     cy.get(loc.BUG_REPORT_PAGE.ALERT_OPERACAO_REALIZADA_SUCESSO).should('be.visible').and('contain', 'Operação realizada com sucesso.');
-    //     cy.url().should('include', 'view.php?id=')
-    // });
+        cy.url().should('include', 'bug_report.php?posted=1')
+        cy.get(loc.BUG_REPORT_PAGE.ALERT_OPERACAO_REALIZADA_SUCESSO).should('be.visible').and('contain', 'Operação realizada com sucesso.');
+        cy.url().should('include', 'view.php?id=')
+    });
 
     it('Criar uma tarefa sem preencher campos do tipo required', () => {
         cy.xpath(loc.MY_VIEW_PAGE.SIDEBAR.LINK_CRIAR_TAREFA).click();
